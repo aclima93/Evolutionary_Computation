787168ebf23087d0f944229ec0d4afada5c5bf10
António Lima <tocalagarto93@gmail.com>
2015-05-07 14:25:27 +0100

    - added the main python project file

---
+++ code/stratego_auto_adapt.py
@@ -0,0 +1,33 @@
+'''
+Topic: Auto-Adaptation in Evolutionary Strategies
+Authors: António Lima & Paulo Pereira
+Remarks: Not for the faint of heart! Procede at your own risk.
+'''
+
+
+# criar a função run_n_times
+# - tem todos os parâmetros que devem ser analisados estatisticamente
+# - executa a função run n vezes
+# - guarda os resultados e parâmetros de execução num ficheiro
+def run_n_times(num_runs):
+	for i in range(num_runs):
+		run()
+    return
+
+
+# criar a função run
+# - executa uma simulação com os parâmetros fornecidos
+# - devolve os resultados da experiência
+def run():
+    return
+
+
+# criar a função de análise estatística e apresentação de gráficos
+# - analisar os melhores resultados e os resultados da média
+# - analisar o efeito das alterações nos parâmetros
+def analyse():
+    return
+
+
+# starting pint for our algorithm
+if __name__ == '__main__':

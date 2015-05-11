4420e9395ae8e0a459e4fbc5517fc3adb3a833c5
António Lima <tocalagarto93@gmail.com>
2015-05-11 17:28:01 +0100

    - added the stratego function

--- code/stratego_auto_adapt.py
+++ code/stratego_auto_adapt.py
@@ -1,12 +1,11 @@
-'''
+"""
 Topic: Auto-Adaptation in Evolutionary Strategies
 Authors: António Lima & Paulo Pereira
-Remarks: Not for the faint of heart! Procede at your own risk.
-'''
+Remarks: This is mostly a wrapper, but it's ours
+"""
 
-from sea_bin import *
-from kp_1 import *
 from random import *
+from kp_1 import *
 
 
 # tendo em conta a janela de referência, decidir se devem ser alterados os parâmetros
@@ -17,17 +16,57 @@ def auto_adapt_fitness():
     return
 
 
+def stratego(numb_generations, size_pop, size_cromo, prob_mut, prob_cross, sel_parents, recombination, mutation,
+             sel_survivors, fitness_func, refference_window):
+
+    # TODO: apply population size alteration + cross & mut prob alteration based on refference window
+
+    # inicializa população: indiv = (cromo,fit)
+    populacao = gera_pop(size_pop, size_cromo)
+    # avalia população
+    populacao = [(indiv[0], fitness_func(indiv[0])) for indiv in populacao]
+
+    for i in range(numb_generations):
+        # selecciona progenitores
+        mate_pool = sel_parents(populacao)
+        # Variation
+        # ------ Crossover
+        progenitores = []
+        for i in range(0, size_pop - 1, 2):
+            cromo_1 = mate_pool[i]
+            cromo_2 = mate_pool[i + 1]
+            filhos = recombination(cromo_1, cromo_2, prob_cross)
+            progenitores.extend(filhos)
+            # ------ Mutation
+        descendentes = []
+        for indiv, fit in progenitores:
+            novo_indiv = mutation(indiv, prob_mut)
+            descendentes.append((novo_indiv, fitness_func(novo_indiv)))
+        # New population
+        populacao = sel_survivors(populacao, descendentes)
+        # Avalia nova _população
+        populacao = [(indiv[0], fitness_func(indiv[0])) for indiv in populacao]
+
+    return best_pop(populacao)
+
+
 # criar a função run
 # - executa uma simulação com os parâmetros fornecidos
 # - devolve os resultados da experiência
-def run(auto_adapt, refference_window, population_size, prob_crossover, prob_mutation):
+def run(auto_adapt, refference_window, problem, size_items, num_generations, population_size, prob_crossover,
+        prob_mutation):
+    if not auto_adapt:
+        # sea(numb_generations, size_pop, size_cromo, prob_mut, prob_cross, sel_parents, recombination,
+        #     mutation, sel_survivors, fitness_func)
+        best = sea(num_generations, population_size, size_items, prob_mutation, prob_crossover, tour_sel(3),
+                   one_point_cross, muta_bin, sel_survivors_elite(0.02), merito(problem))
+    else:
+        # sea(numb_generations, size_pop, size_cromo, prob_mut, prob_cross, sel_parents, recombination,
+        #     mutation, sel_survivors, fitness_func, refference_window)
+        best = stratego(num_generations, population_size, size_items, prob_mutation, prob_crossover, tour_sel(3),
+                        one_point_cross, muta_bin, sel_survivors_elite(0.02), merito(problem), refference_window)
 
-    problem = generate_strong_cor(10, 10, 5)
-    fit = merito(problem)
-    best = sea(100, 50, 10, 0.1, 0.8, tour_sel(3), one_point_cross, muta_bin, sel_survivors_elite(0.02), fit)
-    display(best, phenotype, problem)
-
-    return
+    return [best, phenotype, problem]
 
 
 # criar a função run_n_times
@@ -36,14 +75,22 @@ def run(auto_adapt, refference_window, population_size, prob_crossover, prob_mut
 # --- aquando da mutação e recombinação
 # - executa a função run n vezes
 # - guarda os resultados e parâmetros de execução num ficheiro
-def run_n_times(num_runs, refference_window, population_size, prob_crossover, prob_mutation):
+def run_n_times(num_runs, refference_window, num_generations, population_size, prob_crossover, prob_mutation):
+    size_items = 10
+    max_value = 10
+
     results_with_auto_adapt = []
     results_without_auto_adapt = []
 
     for i in range(num_runs):
-        results_with_auto_adapt.append(run(True, refference_window, population_size, prob_crossover, prob_mutation))
-        results_without_auto_adapt.append(run(False, refference_window, population_size, prob_crossover, prob_mutation))
+        problem = generate_uncor(size_items, max_value)
 
+        results_with_auto_adapt.append(
+            run(True, refference_window, problem, size_items, num_generations, population_size, prob_crossover,
+                prob_mutation))
+        results_without_auto_adapt.append(
+            run(False, refference_window, problem, size_items, num_generations, population_size, prob_crossover,
+                prob_mutation))
 
     return results_with_auto_adapt, results_without_auto_adapt
 
@@ -52,32 +99,31 @@ def run_n_times(num_runs, refference_window, population_size, prob_crossover, pr
 # - analisar os melhores resultados e os resultados da média
 # - analisar o efeito das alterações nos parâmetros
 def analyse(data):
+    for [best, phenotype, problem] in data:
+        display(best, phenotype, problem)
+
     return
 
 
 # starting point for our algorithm
 if __name__ == '__main__':
-
     seed(666)  # random number generation with fixed seed for reproduceable results
 
     number_of_runs = 1  # TODO: 30  # statistically relevant ammount of runs
 
     refference_window = 1  # number previous generations to eb considered for altering the auto-adaptative parameters
 
+    num_generations = 500
     population_size = 250  # the number of elements influences the effect fo reproduction because the gene pool is bigger
-    prob_crossover = 0.70  # resposável por salto grande no início
-    prob_mutation = 0.01  # resposável por saltos pequenos no final
+    prob_crossover = 0.80  # resposável por salto grande no início
+    prob_mutation = 0.10  # resposável por saltos pequenos no final
 
     results_with_auto_adapt, results_without_auto_adapt = run_n_times(number_of_runs, refference_window,
-                                                                      population_size, prob_crossover, prob_mutation)
+                                                                      num_generations, population_size, prob_crossover,
+                                                                      prob_mutation)
 
-    analyse(results_with_auto_adapt)
+    print("\n\n----- Analysing results without Auto-Adapt -----")
     analyse(results_without_auto_adapt)
 
-
-
-
-
-
-
-
+    print("\n\n----- Analysing results with Auto-Adapt -----")
+    analyse(results_with_auto_adapt)
